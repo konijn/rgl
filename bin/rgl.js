28f35037f7b125b192b080dac04f2f306aee4d78
TjD <konijn@gmail.com>
2016-10-05 10:16:17 -0400

    Doors, streams, asynchronous joy &_&
    * Refactored and commented run/runLevel to make the flow clear(er)
    * Support for doors who will ask input before letting you pass
    * Support for providing a stream so that you can test a script with doors by passing an input file
    * Renamed a few functions to stay in the game theme
    * Introduced template to scrolls
    * Introduced the concept of the dingle (next item on a stack)
    * Added support for config-less dungeons

--- bin/rgl.js
+++ bin/rgl.js
@@ -51,7 +51,7 @@ upgradeNode();
 if ( filename ){
   fs.readFile(filename ,(err, data) => {
     if (err) throw err;
-    run( data );
+    runFile( data );
   });
 }else {
   console.error( 'No rgl script name was provided' );
@@ -85,6 +85,12 @@ Layout.prototype.walk = function walk( runtime ){
   var cell = runtime.cell,
       direction = runtime.direction,
       currentVortex = runtime.callStack.last();
+
+  if( !cell ){
+    runtime.cell = runtime.level.layout.getEntry();
+    runtime.direction = { line : 0 , char : 1 };
+    return true;
+  }
   
   if( currentVortex && !currentVortex.cell.equals(cell) && currentVortex.type == 'v' ){
     //Minor time vortexes set you back, do not change direction
@@ -184,6 +190,7 @@ function lex( lines ){
 
   var levels, level;
 
+  //console.log( lines );
   for( let line of lines ){
 
     if(!level){
@@ -210,6 +217,8 @@ function lex( lines ){
       }
     }
   }
+  if( !level.config )
+    level.config = new Config( '' );
 
   return levels;
 }
@@ -224,6 +233,8 @@ function parse( levels ){
   //We parse up front the config
   for( let level of levels ){
 
+    //level.config = level.config || new Config('');
+    //console.log( level.config.lines );
     //Prep config first, I guess?
     for( let line of level.config.lines ){
       
@@ -264,15 +275,22 @@ function execute( runtime ){
       type = runtime.level.layout.lines[cell.line][cell.char],
       script  = runtime.level.layout.scripts[cell.line][cell.char];
 
+ runtime.async = false;
+
   //The switch, it requires break
   if( type == '?' ) scroll( script, runtime );
   else if( type == '&' ) chest( script, runtime );
   else if( type == '~' ) light( script, runtime );
   else if( type == 'Z' ) chaos( script, runtime );
-  else if( type == 'V' ) timeN( script, runtime, 'V' );
-  else if( type == 'v' ) timeN( script, runtime, 'v' );
+  else if( type == 'V' ) timeVortex( script, runtime, 'V' );
+  else if( type == 'v' ) timeVortex( script, runtime, 'v' );
   else if( type == '0' ) jump( script, runtime );
   else if( type == 'R' ) zothOmmog( script, runtime );
+  else if( type == '+' ) door( script, runtime );
+
+  if( !runtime.async )
+    run( runtime );
+    
 }
 
 function Yugg(script){
@@ -280,13 +298,9 @@ function Yugg(script){
   this.script = script;
 }
 
-
 //Zoth Ommog, 'R', third son of Cthulu, caster of Regex, master of Yuggs
 function zothOmmog( script, runtime ){
 
-  var context;
-  console.log( script );
-  console.log( script.shift() );
   if( script.startsWith('&') ){
     runtime.stack.push( new Yugg( script.shift() ) );
   }else{
@@ -295,13 +309,12 @@ function zothOmmog( script, runtime ){
 }
 
 //Retrieve the Yuggs from the stack, apply them to the runtime
-function summonZothOmmog( runtime ){
+function applyYuggs( runtime ){
 
   while( runtime.stack.length ){
     var o = runtime.stack.pop();
     if( o instanceof Yugg ){
       runtime.mods.regexOut.push( o );
-      console.log( o );
     } else {
       runtime.stack.push( o )
       return;
@@ -309,16 +322,57 @@ function summonZothOmmog( runtime ){
   }
 }
 
+//Unlike roguelikes, these doors ask for input before
+//letting you through, I am open to a more roguelike 
+//convention for asking user input
+//It is symbolic that this is 'blocking'
+/*
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+*/
+function door( script, runtime ){
 
+  var input;
 
-function timeN( script, runtime, char ){
+  runtime.async = true;
+  process.stdin.resume();
+  process.stdin.on('data', function (raw) {
+    process.stdin.pause();
+    //console.log('received data:', input = raw.toString(), raw );
+    input = raw.toString().trimRight();
+    if( !runtime.fileMode ){
+      let lines = input.split( runtime.mods.eol );
+      if( lines.length > 1 ){
+        input = lines.shift();
+        process.stdin.push( lines.join( runtime.mods.eol ) );
+      }
+    }
+    if(!script){
+      runtime.stack.push( input )
+    }else if( script == 'a' || script == 'Ò' ){
+      runtime.registers[0] = input;
+    }else if( script == 'b' || script == 'Ó' ){
+      runtime.registers[0] = input;
+    }else if( script == 'c' || script == 'Ô' ){
+      runtime.registers[0] = input;
+    }else if( script == 'd' || script == 'Õ' ){
+      runtime.registers[0] = input;
+    }else if( script == 'e' || script == 'Ö' ){
+      runtime.registers[0] = input;
+    }
+    run( runtime );
+  });
+}
+
+function timeVortex( script, runtime, char ){
 
   var count, originalCount;
 
   if(!script){
-    //console.log(runtime.stack);
     count = runtime.stack.last(runtime)*1;
-    //console.log(count);
   }else if( isNaN( script.charAt(0)  ) ){
     originalCount = runtime.stack.last();
     chaos( script, runtime );
@@ -397,14 +451,15 @@ function light( script, runtime ){
 
   for( let char of script ){
     if(!context){
-      if( char == 'k' ) mods.keep = true;      //k => keep used values on stack
-      if( char == 'l' ) mods.keep = false;     //l => lose used values on stack
+      if( char == 'k' ) mods.keep = true;         //k => keep used values on stack
+      if( char == 'l' ) mods.keep = false;        //l => lose used values on stack
       if( char == 'c' ) mods.separatorOut = '';   //c => concat output (no newlines after scrolls)
-      if( char == 'n' ) mods.separatorOut = '\n'; //n =? newlines after scrolls
+      if( char == 'n' ) mods.separatorOut = '\n'; //n => newlines after scrolls
+      if( char == 'F' ) mods.fileMode = true;     //F => do not split stdin up into newline separated chunks
       if( char == 'S' ) context = 'mods.separatorOut';
       if( char == 's' ) context = 'mods.separator';
       if( char == 'r' ) context = 'mods.separatorRegex';
-      if( char == 'R' ) summonZothOmmog( runtime );
+      if( char == 'R' ) applyYuggs( runtime );
     }else{
       //Todo, allow separator to come from the stack
       if( context == 'mods.separatorOut' ) mods.separatorOut = char;
@@ -441,15 +496,18 @@ function replaceYouMaybe( match, symbol, value ){
                           : value;
 }
 
-
+function templateString( s  , runtime ){
+  //Replace @ with the dingle, but leave \@ alone
+  let dingle = runtime.stack.last();
+  s = s.replace( /^@/g , function f(s){ return replaceYouMaybe( s , '@' , dingle ) });
+  s = s.replace( /.@/g , function f(s){ return replaceYouMaybe( s , '@' , dingle ) });
+  return s;
+}
 
 function write( s , runtime ){
 
  s = s.toString().replace( /\\n/g , runtime.mods.eol );
- let dingle = runtime.stack.last();
- //Replace @ with the dingle, but leave \@ alone
- s = s.replace( /^@/g , function f(s){ return replaceYouMaybe( s , '@' , dingle ) });
- s = s.replace( /.@/g , function f(s){ return replaceYouMaybe( s , '@' , dingle ) });
+ s = templateString( s , runtime );
 
 
  if( runtime.mods.regexOut.length ){
@@ -465,8 +523,6 @@ function write( s , runtime ){
  myProcess.stdout.write( s );
 
 }
-
-
 
 function scroll( script, runtime ){
 
@@ -490,19 +546,6 @@ function scroll( script, runtime ){
 
 }
 
-
-function runLevel( levels, levelIndex, runtime ){
-
-  runtime.level = levels[levelIndex];
-  runtime.cell = runtime.level.layout.getEntry();
-  runtime.direction = { line : 0 , char : 1 };
-
-  do {
-    execute( runtime );
-  }
-  while ( runtime.level.layout.walk( runtime ) );
-}
-
 function Cell( line, char){
   this.line = line;
   this.char = char;
@@ -517,11 +560,12 @@ Cell.prototype.equals = function cellEquals(cell){
 }
 
 
-function Runtime( stack, eol ){
+function Runtime( levels, stack, eol ){
 
   this.stack = stack,
   this.callStack = [],
   this.registers = [],
+  this.levels = levels,
   this.mods = {
     keep : false,
     radixIn : 10,
@@ -530,7 +574,8 @@ function Runtime( stack, eol ){
     separator : ';',
     separatorRegex: '`',
     regexOut: [],
-    eol: eol
+    eol: eol,
+    fileMode: false
   }
 }
 
@@ -540,14 +585,37 @@ function TimeVortex( type, cell, count ){
   this.count = count;
 }
 
-function run( fileContent ){
+//This is the entry point, the calls go
+//runFile -> runLevel -> run <-> execute
+//execute will will call back run for synchronous operations
+//asynchronous operations (like doors) will call run when the time has come
+//run might of course call runLevel, TODO: level stack
+function runFile( fileContent ){
   debugger;
   var fileContent = fileContent.toString(),
       eol = ~fileContent.indexOf('\r\n') ? '\r\n' : '\n',
       lines = fileContent.split( eol ),
       levels = parse( lex( lines ) );
 
-  runLevel( levels, 0, new Runtime( postArgs, eol ) ); 
+  runLevel( 0, new Runtime( levels, postArgs, eol ) ); 
+}
+
+function runLevel( levelIndex, runtime ){
+  //Set the level
+  runtime.level = runtime.levels[levelIndex];
+  //Where are we??
+  runtime.cell = undefined;
+  //Where are we going??
+  runtime.direction = undefined;
+  //Calling run will figure this out
+  run( runtime );
+}
+
+function run( runtime ){
+  //walk & execute or exit
+  if( runtime.level.layout.walk( runtime ) ){
+    execute( runtime );
+  }
 }
 
 
@@ -560,7 +628,7 @@ function upgradeNode(){
     if( typeof runtimeOrKeep == 'boolean' )
       return runtimeOrKeep ? value : this.pop();
     if( runtimeOrKeep instanceof Runtime )
-      return runtimeOrKeep.mods.keep ? value : pop();
+      return runtimeOrKeep.mods.keep ? value : this.pop();
     console.warn( 'Array.last() called not using undefined/boolean/Runtime: ' + JSON.stringify( Object.getPrototypeOf( runtimeOrKeep ) ) );
     return value;
   }
